b019d79e39befbda89e6184d747371ae4b9c68d8
Jorge <luisgarrido0987@gmail.com>
2022-07-17 22:15:13 -0500

    telegram security added

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var fs = require("fs");
-
+var TelegramBot = require("node-telegram-bot-api");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 //
+//const token = '5595672851:AAF0e6T-nvOkjujxguT9UrO9ldalczegIko';
 const folder = "./components/users/";
 const filename = "./data.json";
 const rawdata = fs.readFileSync(filename);
 const student = JSON.parse(rawdata);
+const token = student.token;
+const bot = new TelegramBot(token, { polling: true });
 //
 app.use(cors());
 app.use(morgan("dev"));
@@ -32,43 +35,80 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 //
-app.post("/login", (req, res) => {
-   var user = req.body.user
-   var pass = req.body.pass
-   var a = folder + user + "/data.json";
-   if (!fs.existsSync(a)) {
+app.post("/download", (req, res) => {
+  var user = req.body.user;
+  var folderNameByUser = folder + user + "/data.json";
+  if (!fs.existsSync(folderNameByUser)) {
     res.json({
-      title: "Error", 
+      title: "Error",
       data: "No se econtro información del usuario",
-      message: "error", 
-      extra: 100
+      message: "error",
+      extra: 100,
+    });
+  }
+  var credentials = JSON.parse(fs.readFileSync(folderNameByUser).toString());
+  var telegramId = credentials.chatIdTelegram;
+  try {
+    bot.sendDocument(telegramId, folderNameByUser); 
+    res.json({
+      title: "Todo bien",
+      data: "Se envio la copia de tus datos a tu telegram",
+      message: "success",
+      extra: 205,
+    });
+    return true;
+  } catch (error) {
+    res.json({
+      title: "Todo mal",
+      data: "Hubo un error al intentar enviar los datos al telegrma",
+      message: "error",
+      extra: 205,
+    });
+  }
+    
+})
+
+
+app.post("/login", (req, res) => {
+  var user = req.body.user;
+  var pass = req.body.pass;
+  var a = folder + user + "/data.json";
+  if (!fs.existsSync(a)) {
+    res.json({
+      title: "Error",
+      data: "No se econtro información del usuario",
+      message: "error",
+      extra: 100,
     });
     //HOLA
   }
   var credentials = JSON.parse(fs.readFileSync(a).toString());
-  if(pass===credentials.secondName) {
+  if (pass === credentials.secondName) {
+    var telegramId = credentials.chatIdTelegram;
+    var sms ="Nuevo inicio de sesión en Mymoney.dev";
+    bot.sendMessage(telegramId, sms);
     res.json({
-      title: "success", 
+      title: "success",
       data: "User valido",
-      message: "error", 
+      message: "error",
       extra: 205,
-      token: user
+      token: user,
     });
     return true;
   } else {
+    var telegramId = credentials.chatIdTelegram;
+    var sms ="¡ADVERTENCIA! alguien esta intentando de entrar a tu cuenta de Myoney.dev";
+    bot.sendMessage(telegramId, sms);
     res.json({
-      title: "Error", 
+      title: "Error",
       data: "Es posible que la contraseña este mal",
-      message: "error", 
+      message: "error",
       extra: 107,
-      token: user
+      token: user,
     });
     return true;
   }
-
-})
-
-
+});
 
 app.post("/edit", (req, res) => {
   console.log(req.body);
@@ -79,10 +119,10 @@ app.post("/edit", (req, res) => {
   var a = folder + toEdit.name + "/data.json";
   if (!fs.existsSync(a)) {
     res.json({
-      title: "Error", 
+      title: "Error",
       data: "No se econtro información del usuario",
-      message: "error", 
-      extra: 100
+      message: "error",
+      extra: 100,
     });
   }
   var credentials = JSON.parse(fs.readFileSync(a).toString());
@@ -116,6 +156,19 @@ app.post("/edit", (req, res) => {
   });
   fs.writeFileSync(a, JSON.stringify(credentials));
   if (s < valueEdit) {
+    var telegramId = credentials.chatIdTelegram;
+    var sms =
+      "¡El movimiento de dinero tiene un valor por encima del permitido!";
+    var jsonToSend =
+      "El maximo de " +
+      nameEdit +
+      " es de: " +
+      s +
+      " y se acredito " +
+      valueEdit +
+      " a su rubro.";
+    bot.sendMessage(telegramId, sms);
+    bot.sendMessage(telegramId, jsonToSend);
     res.json({
       data: "El valor excede el maximo permitido " + valueEdit + " < " + s,
       extra: 102,
@@ -146,7 +199,8 @@ app.post("/udapteDebts", (req, res) => {
   console.log(1);
   var f = folder + name + "/data.json";
   if (!fs.existsSync(f)) {
-    res.json({ data: "No se econtro información del usuario", extra: 100 }); return true;
+    res.json({ data: "No se econtro información del usuario", extra: 100 });
+    return true;
   }
   console.log(2);
   var credentials = JSON.parse(fs.readFileSync(f).toString());
@@ -173,7 +227,12 @@ app.post("/udapteDebts", (req, res) => {
 
   if (arrNumber === null) {
     console.log("no se econtro", to);
-    res.json({title: "Cuenta no existe", data: "Deudor no encontrado", extra: 103, message:"error" });
+    res.json({
+      title: "Cuenta no existe",
+      data: "Deudor no encontrado",
+      extra: 103,
+      message: "error",
+    });
     return true;
   }
   if (accountsToacreditCash.includes(from) === false) {
@@ -190,86 +249,99 @@ app.post("/udapteDebts", (req, res) => {
     }
   };
   console.log(7);
-    if (action === "in") {
-      if (value > account()[1]) {
-        res.json({
-          title: "Saldo insuficiente en: " + from,
-          data: "No existen fondos suficientes",
-          extra: 105,
-          message: "error",
-        });
-        return true;
-      }
-      // AÑADIDO NUEVO
-      var mount = deb[arrNumber].mount;
-      var paid = deb[arrNumber].paid;
-      //var after = (credentials.debts[arrNumber].paid = before + value);
-      // AÑADIDO NUEVO
-      if (from === "patrimonio") {
-        // AÑADIDO NUEVO
-        if (mount - paid <= value) {
-          var sum2 = paid - mount + value; //1
-          var ToRes = mount - paid //60
-          //credentials.debts.filter((value) => {value.name != name });
-          
-          var beforeCash = credentials.restOfLastWeek[1].value;
-          credentials.debts[arrNumber].paid= ToRes + paid;
-          credentials.debts.splice(arrNumber, arrNumber+1)
-          credentials.restOfLastWeek[1].value = beforeCash + sum2;
-  
-          fs.writeFileSync(f, JSON.stringify(credentials));
-          console.log("sdasdas");
-          
-          res.json({
-            data:
-              "La deuda se terminó, se acredito $" + sum2 + " al patrimonio",
-            extra: 201,
-            message:"success"
-          });
-          return true
-        }
-
-        var before = credentials.debts[arrNumber].paid;
-        credentials.debts[arrNumber].paid = before + value;
-        var a =  credentials.restOfLastWeek[1].value
-        credentials.restOfLastWeek[1].value = a - value
-        // AÑADIDO NUEVO
-        fs.writeFileSync(f, JSON.stringify(credentials));
-         res.json({
-          title: "Movimiento exitoso",
-          data: "Se modifico la tasa a pagar " + (before - value),
-          extra: 202,
-          message: "success"
-        })
-        return true
-      } else if (from === "savings") {
-        var beforeCash = credentials.savings[1].value;
-        credentials.savings[1].value = beforeCash - value;
-        fs.writeFileSync(f, JSON.stringify(credentials));
-        res.json({
-          title: "Movimiento exitoso",
-          data: "Se modifico la tasa a pagar " + (before - value),
-          extra: 202,
-          message: "success"
-        })
-        return true;
-      }
-    } else if (action === "de") {
-      var beforee = deb[arrNumber].mount;
-      credentials.debts[arrNumber].mount = beforee + value;
-      fs.writeFileSync(f, JSON.stringify(credentials));
+  if (action === "in") {
+    if (value > account()[1]) {
       res.json({
-        data:
-          "Se incremento el valor de " +
-          deb[arrNumber].name +
-          " a " +
-          beforee +
-          value,
-        extra: 203,
+        title: "Saldo insuficiente en: " + from,
+        data: "No existen fondos suficientes",
+        extra: 105,
+        message: "error",
       });
       return true;
     }
-   /*catch (error) {
+    // AÑADIDO NUEVO
+    var mount = deb[arrNumber].mount;
+    var paid = deb[arrNumber].paid;
+    //var after = (credentials.debts[arrNumber].paid = before + value);
+    // AÑADIDO NUEVO
+    if (from === "patrimonio") {
+      // AÑADIDO NUEVO
+      if (mount - paid <= value) {
+        var sum2 = paid - mount + value; //1
+        var ToRes = mount - paid; //60
+        //credentials.debts.filter((value) => {value.name != name });
+
+        var beforeCash = credentials.restOfLastWeek[1].value;
+        credentials.debts[arrNumber].paid = ToRes + paid;
+        credentials.debts.splice(arrNumber, arrNumber + 1);
+        credentials.restOfLastWeek[1].value = beforeCash + sum2;
+
+        fs.writeFileSync(f, JSON.stringify(credentials));
+        console.log("sdasdas");
+        var telegramId = credentials.chatIdTelegram;
+        var sms =
+          "¡Felicidades! la deuda con: " +
+          deb[arrNumber].name +
+          " se ha terminado";
+        var jsonToSend =
+          "Movimiento de dinero: Se acredito: " +
+          value +
+          " a la deuda pendiente de: " +
+          ToRes +
+          " y se delvolvió: " +
+          sum2 +
+          " al patrimonio/savings.";
+        bot.sendMessage(telegramId, sms);
+        bot.sendMessage(telegramId, jsonToSend);
+        res.json({
+          data: "La deuda se terminó, se acredito $" + sum2 + " al patrimonio",
+          extra: 201,
+          message: "success",
+        });
+        return true;
+      }
+
+      var before = credentials.debts[arrNumber].paid;
+      credentials.debts[arrNumber].paid = before + value;
+      var a = credentials.restOfLastWeek[1].value;
+      credentials.restOfLastWeek[1].value = a - value;
+      // AÑADIDO NUEVO
+      fs.writeFileSync(f, JSON.stringify(credentials));
+      res.json({
+        title: "Movimiento exitoso",
+        data: "Se modifico la tasa a pagar " + (before - value),
+        extra: 202,
+        message: "success",
+      });
+      return true;
+    } else if (from === "savings") {
+      var beforeCash = credentials.savings[1].value;
+      credentials.savings[1].value = beforeCash - value;
+      fs.writeFileSync(f, JSON.stringify(credentials));
+      res.json({
+        title: "Movimiento exitoso",
+        data: "Se modifico la tasa a pagar " + (before - value),
+        extra: 202,
+        message: "success",
+      });
+      return true;
+    }
+  } else if (action === "de") {
+    var beforee = deb[arrNumber].mount;
+    credentials.debts[arrNumber].mount = beforee + value;
+    fs.writeFileSync(f, JSON.stringify(credentials));
+    res.json({
+      data:
+        "Se incremento el valor de " +
+        deb[arrNumber].name +
+        " a " +
+        beforee +
+        value,
+      extra: 203,
+    });
+    return true;
+  }
+  /*catch (error) {
     res.json({
       data: "Error critico " + err,
       extra: 000,
@@ -304,7 +376,7 @@ app.post("/acredit", (req, res) => {
     res.json({
       data: "Se edito la informacion, patrimonio: " + (b + value),
       message: "success",
-      extra: 204
+      extra: 204,
     });
   } else if (select === "savings") {
     var b = credentials.savings[1].value;
@@ -313,20 +385,17 @@ app.post("/acredit", (req, res) => {
     res.json({
       data: "Se edito la informacion, total ahorro: " + (b + value),
       message: "success",
-      extra: 204
+      extra: 204,
     });
   } else {
-    res.json({ data: "No se selecciono una cuenta valida", message: "alert", extra: 106 });
+    res.json({
+      data: "No se selecciono una cuenta valida",
+      message: "alert",
+      extra: 106,
+    });
     return true;
   }
 });
-
-
-
-
-
-
-
 
 //
 
